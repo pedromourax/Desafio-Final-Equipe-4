d4779a3cded31ed368e467e46a474d228f8a00ab
Felipe Ferreira <fi.felipe@hotmail.com>
2023-11-27 17:18:50 -0300

    Condiguração do middleware verificarLogin

---
+++ src/controllers/users/efetuarLogin.js
@@ -0,0 +1,8 @@
+const knex = require("../../database/conexao");
+const { senhaCriptografada } = require("../../utilis/criptografia");
+
+const loginUsuario = async (req, res) => {
+
+}
+
+module.exports = loginUsuario;

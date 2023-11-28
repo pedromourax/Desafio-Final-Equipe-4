1ed6d92abc10a6bde0f3168c31ccb4ed4eb3b392
pedromourax <pedrolucas.moura@hotmail.com>
2023-11-27 21:06:38 -0300

    Adicao da rota detalhar perfil

--- src/router/router.js
+++ src/router/router.js
@@ -5,10 +5,16 @@ const validarEmail = require("../middlewares/validarEmail");
 const validarUsuario = require("../middlewares/validarUsuario");
 const validarLogin = require("../middlewares/verificarLogin");
 const loginUsuario = require("../controllers/users/efetuarLogin");
+const verificarToken = require("../middlewares/verificarToken");
+const detalharPerfil = require("../controllers/users/detalharPerfil");
 
 const router = express();
 
 router.post("/usuario", validarEmail, validarUsuario, cadastrarUsuario);
-router.post("/login", validarLogin, loginUsuario)
+router.post("/login", validarLogin, loginUsuario);
+
+router.use(verificarToken);
+
+router.get("/usuario", detalharPerfil);
 
 module.exports = router;

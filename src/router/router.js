0d16366dac5d8151db41b29f4814b832fac625d0
Fabio Bernardo <fabio.bernardom@hotmail.com>
2023-11-28 09:55:18 -0300

    2° commit fabio

--- src/router/router.js
+++ src/router/router.js
@@ -5,10 +5,12 @@ const validarEmail = require("../middlewares/validarEmail");
 const validarUsuario = require("../middlewares/validarUsuario");
 const validarLogin = require("../middlewares/verificarLogin");
 const loginUsuario = require("../controllers/users/efetuarLogin");
+const { editarUsuario } = require("../controllers/users/editarPerfil");
 
 const router = express();
 
 router.post("/usuario", validarEmail, validarUsuario, cadastrarUsuario);
-router.post("/login", validarLogin, loginUsuario)
+router.post("/login", validarLogin, loginUsuario);
+router.put("/usuario/:id", editarUsuario);
 
 module.exports = router;
--- src/utilis/criptografia.js
+++ src/utilis/criptografia.js
@@ -4,12 +4,6 @@ const senhaCriptografada = async (senha, salt = 10) => {
   return await bcrypt.hash(senha, salt);
 };
 
-const senhaDescriptografada = async (senha, senhaBanco) => {
-  const senhaValida = await bcrypt.compare(senha, senhaBanco);
-  return senhaValida;
-};
-
 module.exports = {
   senhaCriptografada,
-  senhaDescriptografada,
 };

fb5c0c87614bd3af061cfd8651bcd89707adccd6
pedromourax <pedrolucas.moura@hotmail.com>
2023-11-28 16:21:26 -0300

    Verificacao com o Joi

---
+++ src/middlewares/validarCorpo.js
@@ -0,0 +1,10 @@
+const validarCorpo = (schema) => async (req, res, next) => {
+    try {
+        await schema.validateAsync(req.body);
+        next();
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message });
+    }
+};
+
+module.exports = validarCorpo;

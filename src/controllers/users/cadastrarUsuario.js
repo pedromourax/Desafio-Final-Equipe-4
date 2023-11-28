df36e71f0b325aa2ae4241517a37ef55d96d4bd8
pedromourax <pedrolucas.moura@hotmail.com>
2023-11-28 16:21:51 -0300

    Verificar corpo da requisicao com o Joi

--- src/controllers/users/cadastrarUsuario.js
+++ src/controllers/users/cadastrarUsuario.js
@@ -1,35 +1,38 @@
 const knex = require("../../database/conexao");
-const { senhaCriptografada } = require("../../utilis/criptografia");
+const criptografarSenha = require("../../utils/criptografarSenha");
 
 const cadastrarUsuario = async (req, res) => {
-  const { nome, email, senha } = req.body;
-  try {
-    if (req.validarEmail) {
-      return res.status(400).json({
-        mensagem: "Já existe usuário cadastrado com o e-mail informado.",
-      });
-    }
+    const { nome, email, senha } = req.body;
+    try {
+        const emailExiste = await knex("usuarios")
+            .where({ email })
+            .returning("*")
+            .first();
 
-    const senhaHash = await senhaCriptografada(senha);
-    const novoUsuario = {
-      nome,
-      email,
-      senha: senhaHash,
-    };
-    const cadastrarUsuario = await knex("usuarios")
-      .insert(novoUsuario)
-      .returning("*");
+        if (emailExiste) {
+            return res.status(400).json({ mensagem: "Email já existe!" });
+        }
 
-    if (cadastrarUsuario.length === 0) {
-      return res.status(400).json("O usuário não foi cadastrado.");
+        const senhaHash = await criptografarSenha(senha);
+        const novoUsuario = {
+            nome,
+            email,
+            senha: senhaHash,
+        };
+        const cadastrarUsuario = await knex("usuarios")
+            .insert(novoUsuario)
+            .returning("*");
+
+        if (cadastrarUsuario.length === 0) {
+            return res.status(400).json("O usuário não foi cadastrado.");
+        }
+        delete cadastrarUsuario[0].senha;
+        return res.status(201).json(cadastrarUsuario[0]);
+    } catch (error) {
+        return res.status(400).json({ mensagem: error.message });
     }
-    delete cadastrarUsuario[0].senha;
-    return res.status(201).json(cadastrarUsuario[0]);
-  } catch (error) {
-    return res.status(400).json({ mensagem: error.message });
-  }
 };
 
 module.exports = {
-  cadastrarUsuario,
+    cadastrarUsuario,
 };

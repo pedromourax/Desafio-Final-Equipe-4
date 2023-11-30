5d76c0463e91f0f5f43495908a535bd4e643c812
Felipe Ferreira <fi.felipe@hotmail.com>
2023-11-30 14:38:52 -0300

    Atualização do schema do banco de dados

--- src/database/dump.sql
+++ src/database/dump.sql
@@ -24,3 +24,24 @@ values
 ('Moda'),
 ('Bebê'),
 ('Games')
+
+create table produtos (
+    id serial primary key,
+    descricao text,
+    quantidade_estoque int not null,
+    valor int not null,
+    categoria_id int references categorias(id)
+);
+
+create table clientes (
+    id serial primary key,
+    nome text not null,
+    email text not null unique,
+    cpf int not null unique,
+    cep int not null,
+    rua text not null,
+    numero int not null,
+    bairro text not null,
+    cidade text not null,
+    estado text not null
+);

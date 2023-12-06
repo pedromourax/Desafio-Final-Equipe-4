ce2c1583a218411f1d1246f171f3c60a5cc7eb37
pedromourax <pedrolucas.moura@hotmail.com>
2023-12-06 17:17:01 -0300

    Adicionado filtro de categoria

--- src/controllers/product/listarProdutos.js
+++ src/controllers/product/listarProdutos.js
@@ -1,12 +1,24 @@
 const knex = require("../../database/conexao");
 
 const listarProdutos = async (req, res) => {
+    const { categoria_id } = req.query
+    console.log(categoria_id)
     try {
-        const produtos = await knex("produtos").select("*");
+        let produtos;
+        if (!categoria_id) {
+            produtos = await knex("produtos").select("*")
+        }
+        else if (categoria_id.length == 1) {
+            produtos = await knex("produtos").select("*").where({ categoria_id })
+        }
+        else {
+            produtos = await knex("produtos").select("*").whereIn('categoria_id', categoria_id)
+        }
+
         return res.status(200).json(produtos);
     } catch (error) {
         return res.status(400).json({ mensagem: error.message });
     }
 }
 
-module.exports = listarProdutos;    
+module.exports = listarProdutos;

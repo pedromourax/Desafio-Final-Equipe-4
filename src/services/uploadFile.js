6edd3c22ead7a2a5cde1c07b7b036b4c79efaad5
pedromourax <pedrolucas.moura@hotmail.com>
2023-12-12 17:48:11 -0300

    Correcao de erros para enviar arquivos

--- src/services/uploadFile.js
+++ src/services/uploadFile.js
@@ -1,13 +1,23 @@
-const s3 = require("./aws-s3")
+const aws = require("aws-sdk")
+
+const endpoint = new aws.Endpoint('s3.us-east-005.backblazeb2.com')
+
+const s3 = new aws.S3({
+    endpoint,
+    credentials: {
+        accessKeyId: process.env.KEY_ID,
+        secretAccessKey: process.env.APP_KEY
+    }
+})
 
 const uploadFile = async (path, buffer, mimetype) => {
+
     const arquivo = await s3.upload({
         Bucket: process.env.BUCKET_NAME,
         Key: path,
         Body: buffer,
         ContentType: mimetype
-    }).promisse()
-
+    }).promise()
     return {
         url: arquivo.Location,
         path: arquivo.Key
